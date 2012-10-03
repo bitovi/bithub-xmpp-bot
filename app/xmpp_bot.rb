f8a3221db337b1c014d774864b5355083d4635f7
Nikica Jokic <neektza@gmail.com>
2012-10-03 23:49:53 +0200

    XHTML in messages.

--- app/xmpp_bot.rb
+++ app/xmpp_bot.rb
@@ -30,15 +30,39 @@ module Bot
 
   # Echo back what was said
   message :chat?, :body do |m|
-    say m.from, "You said: #{m.body}"
+
+
   end
 
   def self.say_to_roster(payload)
-    msg = Yajl::Parser.parse(payload)
+    json = Yajl::Parser.parse(payload)
     my_roster.items.each do |item|
-      say item[0], "#{msg['actor']} : #{msg['title']}"
+
+      body = <<-body
+<body>
+#{json['actor']} : #{json['title']} : #{json['feed'].upcase} : #{json['link']}
+</body>
+      body
+
+      xhtml = <<-xhtml
+<html xmlns='http://jabber.org/protocol/xhtml-im'>
+<body xmlns='http://www.w3.org/1999/xhtml'>
+<p>
+#{json['actor']} : #{json['title']} : <a href='#{json['link']}'>#{json['feed'].upcase}</a>
+#</p>
+</body>
+</html>
+      xhtml
+
+      msg = Blather::Stanza::Message.new
+      msg.body = body 
+      msg.xhtml = xhtml 
+      msg.to = item[0]
+
+      client.write msg
     end
   end
+
 end
 
 EM.run do
